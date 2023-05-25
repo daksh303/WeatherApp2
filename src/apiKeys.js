2ccab7b981f268fe16d09adc18b3d330cad00ed2
daksh303 <pruthidaksh.dp303@gmail.com>
2023-05-26 01:23:10 +0530

    weatherApp project uploaded

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "6880267f76d0643812d28187df23d189",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
